c798d0c3b52b30935221eb2a27b8c9a3cd92cd3c
Dokyun Lim <im.koremp@gmail.com>
2020-07-23 15:26:48 +0900

    remove main func

--- yatch.py
+++ yatch.py
@@ -1,7 +1,5 @@
 import random
 
-CATEGORY_NAME = ["Aces", "Duces", "Threes", "Fours", "Fives", "Sixes"]
-
 
 def four_of_a_kind(dice_list):
     fok = 0
@@ -127,90 +125,3 @@ def yacht(dice_list):
         yc = 50
 
     return yc
-
-
-def main():
-    field_dice_list = []
-    saved_dice_list = []
-    total_dice_list = []
-    upper_section = [0 for x in range(6)]
-    selected_upper_section = [0 for x in range(6)]
-    fok, fh, ss, ls, yc = 0, 0, 0, 0, 0
-
-    # roll dice
-    for x in range(5):
-        total_dice_list.append(random.randrange(1, 7))
-
-    total_dice_list.sort()
-
-    # calc upper section
-    for dice in total_dice_list:
-        if dice == 1:
-            upper_section[0] = upper_section[0] + 1
-        elif dice == 2:
-            upper_section[1] = upper_section[1] + 1
-        elif dice == 3:
-            upper_section[2] = upper_section[2] + 1
-        elif dice == 4:
-            upper_section[3] = upper_section[3] + 1
-        elif dice == 5:
-            upper_section[4] = upper_section[4] + 1
-        elif dice == 6:
-            upper_section[5] = upper_section[5] + 1
-
-    # calc lower section
-
-    fok = four_of_a_kind(total_dice_list)
-    fh = full_house(total_dice_list)
-    ss = small_straight(total_dice_list)
-    ls = large_straight(total_dice_list)
-    yc = yacht(total_dice_list)
-
-    # print upper section
-    print("Num")
-    for i in range(6):
-        if selected_upper_section[i] == 0:
-            print(f'{CATEGORY_NAME[i]} - {upper_section[i] * (i + 1)}')
-        else:
-            print(
-                f'{CATEGORY_NAME[i]} - {selected_upper_section[i] * (i + 1)}')
-
-    # print lower section
-    print(f'Choices - {sum(total_dice_list)}')
-    print(f'4 of a Kind - {fok}')
-    print(f'Full House - {fh}')
-    print(f'S. Straight - {ss}')
-    print(f'L. Straight - {ls}')
-    print(f'Yacht - {yc}')
-
-    while True:
-        key = input('1 ~ 5 to save dice / r to roll / s to score / ')
-
-        if key == '1':
-            saved_dice_list.append(field_dice_list.pop([0]))
-
-        elif key == '2':
-            saved_dice_list.append(field_dice_list.pop([1]))
-
-        elif key == '3':
-            saved_dice_list.append(field_dice_list.pop([2]))
-
-        elif key == '4':
-            saved_dice_list.append(field_dice_list.pop([3]))
-
-        elif key == '5':
-            saved_dice_list.append(field_dice_list.pop([4]))
-
-        elif key == 'r':
-            field_dice_num = len(field_dice_list)
-            field_dice_list.clear()
-
-            for x in range(field_dice_num):
-                field_dice_list.append(random.range(1, 7))
-
-        elif key == 's':
-            '''  '''
-
-
-if __name__ == '__main__':
-    main()
